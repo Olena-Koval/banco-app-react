d0dfd7fcb5293d942a9970e9e5de20f554f7ea25
Olena-Koval <19111982e@gmail.com>
2025-02-24 11:11:17 +0100

    Los cambios en ficheros [200~package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/banco-app-react/", // 🔹 Agregamos la base correcta para GitHub Pages
   plugins: [react()],
-})
+});
